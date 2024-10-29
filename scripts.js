6fe3ad142bd9636a81904bcf582028e9eb966d89
Franpall <98850575+Franpall@users.noreply.github.com>
2024-10-29 13:44:50 -0400

    Optimización de recursos

--- scripts.js
+++ scripts.js
@@ -4,16 +4,28 @@ function zoom(num_foto){
         document.getElementById(identificador).className = "zoom";
     }
     else{
-        document.getElementById(identificador).className = "nozoom"
+        document.getElementById(identificador).className = "nozoom";
     }
 }
 
 function hacerZoomUsuario(){
-    elemento = document.getElementById("zoom")
+    elemento = document.getElementById("zoom");
+    mensaje = document.getElementById("mensajeImg");
     if (elemento.className == "imagen_usuario"){
-        elemento.className = "imagen_usuarioZoom" 
+        elemento.className = "imagen_usuarioZoom";
+        mensaje.className = "mensajeImgOn";
     }
     else{
-        elemento.className = "imagen_usuario"      
+        elemento.className = "imagen_usuario";      
+        mensaje.className = "mensajeImgOff"; 
     }
+}
+
+function redireccionar(destino){
+    switch (destino){
+        case "telegram":
+            window.open("https://t.me/franpal3");
+        break;
+    }
+
 }
